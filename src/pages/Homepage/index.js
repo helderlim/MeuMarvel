a16d027a9461e7f75851740469ff57bcc95e312b
helderlim <helderfelima16@gmail.com>
2021-05-30 21:37:37 -0300

    feat: create component

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import ListCommic from '../../components/ListCommics';
 
 export default ({ title, items }) => {
     console.log(items.data.results);
     return (
         <div>
-            <h2>{title} </h2>
-            {items.data.results.length > 0 && items.data.results.map((item, key) => (
-                <div key={key}>
-                    <h2>{item.title}</h2>
-                    <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} />
-                </div>
-            ))}
+            <ListCommic
+                items={items}
+                title={title}
+                />         
+           
         </div>
     )
 }
