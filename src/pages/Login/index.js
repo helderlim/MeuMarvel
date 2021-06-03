73f4263127d7b616ee12babfbdb4017ff5822893
helderlim <helderfelima16@gmail.com>
2021-06-03 00:36:02 -0300

    feat: tela login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
+import { Container, Title, InputLogin, InputPassword, ButtonLogin, Box, Form, Label } from './styles';
 
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <Container>
+            <Box>
+                <Form>
+                    <Title>MeuMarvel.com</Title>
+                    <Label>Login</Label>
+                    <InputLogin />
+                    <Label>Senha</Label>
+                    <InputPassword type="password"/>
+                </Form>
+               
+                <Route render={({ history}) => ( <ButtonLogin onClick={() => { history.push('/homepage') }}>Logar</ButtonLogin>
+                )} />
+            </Box>
+
+        </Container>
+
     )
 }
 
-export default Login; 
+export default Login;
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,2 +1,37 @@
+import styled from "styled-components";
 
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 90vh;
+`;
+export const Form = styled.div`
+   
+    color: white;
+    text-align: center;
+    height: 230px;
+    width: 300px;
+`;
+export const Box = styled.div`
+    background-color: black;
+    
 
+`;
+export const Label = styled.p`
+
+`;
+export const Title = styled.p`
+    font-size: 30px;    
+`;
+export const InputLogin = styled.input`
+
+`;
+export const InputPassword = styled.input`
+
+`;
+export const ButtonLogin = styled.button`
+    height: 30px;
+    width: 90px;
+    margin-left: 110px;
+`;
